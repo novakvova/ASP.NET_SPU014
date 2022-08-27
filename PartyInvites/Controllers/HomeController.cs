65f4be61b00be34bb4a622471431f9c8fbe920e4
novakvova <novakvova@gmail.com>
2022-08-27 12:54:02 +0300

    Good working app

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -1,12 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+using System.Linq;
 
 namespace PartyInvites.Controllers
 {
     public class HomeController : Controller
     {
+        private DataContext context;
+        public HomeController(DataContext context)
+        {
+            this.context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Respond() => View();
+
+        [HttpPost]
+        public IActionResult Respond(GuestResponse response)
+        {
+            context.Responses.Add(response);
+            context.SaveChanges();
+            return RedirectToAction(nameof(Thanks),
+                new {Name=response.Name, WillAttend = response.WillAttend });
+        }
+        public IActionResult Thanks(GuestResponse response) => View(response);
+
+        public IActionResult ListResponses()
+        {
+            return View(context.Responses.OrderByDescending(r => r.WillAttend));
         }
     }
 }
